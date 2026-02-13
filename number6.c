4d244243167736a9861e65e025cf67a1eca358b7
Ishkhan <mkrtchyanishxan6@gmail.com>
2026-02-13 23:12:28 +0400

    Update number6.c

--- number6.c
+++ number6.c
@@ -11,12 +11,23 @@ typedef struct Vector{
     size_t capacity;
 }Vector;
 
+void resize(Vector *vector);
 void push_front(Vector *vector, TYPE value);
 
 int main(){
 
 }
 
-void pop_back(Vector *vector){
-        --vector->size;
+void resize(Vector *vector){
+        if(vector->size == vector->capacity){
+                reserve(vector, vector->capacity * 2);
+}
+
+}
+
+void push_front(Vector *vector, TYPE value){
+        resize(vector);
+        for(int i = vector->size; i > 0; --i){
+    vector->ptr[i] = vector->ptr[i - 1];
+        }
 }
