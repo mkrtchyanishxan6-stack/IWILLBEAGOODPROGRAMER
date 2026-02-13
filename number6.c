e01a81df53a4e8112bbd76d41019f4e8626ba005
Ishkhan <mkrtchyanishxan6@gmail.com>
2026-02-13 23:16:28 +0400

    Update number6.c

--- number6.c
+++ number6.c
@@ -11,11 +11,23 @@ typedef struct Vector{
     size_t capacity;
 }Vector;
 
+bool reserve(Vector *vector, size_t size);
 void resize(Vector *vector);
 void push_front(Vector *vector, TYPE value);
 
 int main(){
 
+}
+
+bool reserve(Vector *vector, size_t size){
+        TYPE *tmp = realloc(vector->ptr, size * sizeof(TYPE));
+if(tmp == NULL){
+    perror("Realloc error");
+    return false;
+}
+        vector->ptr = tmp;
+        vector->capacity = size;
+        return true;
 }
 
 void resize(Vector *vector){
