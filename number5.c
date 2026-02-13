b25b3230a70b15a82eb5e408dc8759b827293642
Ishkhan <mkrtchyanishxan6@gmail.com>
2026-02-13 22:58:34 +0400

    Create number5.c

---
+++ number5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TYPE int
+#define DEFAULT_SIZE 8
+
+typedef struct Vector{
+    TYPE*ptr;
+    size_t size;
+    size_t capacity;
+}Vector;
+
+bool reserve(Vector *vector, size_t size);
+void resize(Vector *vector);
+void push_back(Vector *vector, TYPE value);
+
+int main(){
+
+}
+
+bool reserve(Vector *vector, size_t size){
+        TYPE *tmp = realloc(vector->ptr, size * sizeof(TYPE));
+if(tmp == NULL){
+    perror("Realloc error");
+    return false;
+}
+vector->ptr = tmp;
+
+
+        vector->capacity = size;
+        return true;
+
+
+}
+
+void resize(Vector *vector){
+        if(vector->size == vector->capacity){
+                reserve(vector, vector->capacity * 2);
+}
+
+}
+
+void push_back(Vector *vector, TYPE value){
+        resize(vector);
+        vector->ptr[vector->size] = value;
+        ++vector->size;
+}
